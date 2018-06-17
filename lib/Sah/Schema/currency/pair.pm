c153291e30fccf6a73d961f4bfba57d0e6e648fa
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-06-18 05:41:03 +0700

    Add schema: currency::pair

---
+++ lib/Sah/Schema/currency/pair.pm
@@ -0,0 +1,25 @@
+package Sah::Schema::currency::pair;
+
+# DATE
+# VERSION
+
+our $schema = [str => {
+    summary => 'Fiat currency pair, e.g. USD/IDR',
+    description => <<'_',
+
+Currency pair is string in the form of *currency1*/*currency2*, where
+*currency1* is called the base currency while *currency2* is the quote (or
+price) currency. Both must be known currency codes (e.g. USD, or IDR).
+
+Currency code is checked against <pm:Locale::Codes::Currency_Codes>.
+
+Will be normalized to uppercase.
+
+_
+    match => qr(\A\S+/\S+\z),
+    #'x.completion' => 'currency_pair',
+    'x.perl.coerce_rules' => ['str_to_currency_pair'],
+}, {}];
+
+1;
+# ABSTRACT:
